01177fbb1cb6e46f47e236792ea4b7a041a2cdc6
10218504 <10218504chen_ke@cn.tre-inc.com>
2022-03-17 11:01:07 +0800

    放置商品部分加上详细注释

--- src/main/java/com/trechina/planocycle/service/Impl/PriorityOrderMstServiceImpl.java
+++ src/main/java/com/trechina/planocycle/service/Impl/PriorityOrderMstServiceImpl.java
@@ -89,9 +89,6 @@ public class PriorityOrderMstServiceImpl implements PriorityOrderMstService {
     private cgiUtils cgiUtil;
     @Autowired
     private ShelfPtsDataMapper shelfPtsDataMapper;
-    @Autowired
-    private WorkPriorityOrderMstMapper workPriorityOrderMstMapper;
-
 
     /**
      * 获取优先顺位表list
@@ -989,11 +986,10 @@ public class PriorityOrderMstServiceImpl implements PriorityOrderMstService {
             partitionVal = 2;
         }
 
-        /**
-         * 根据制约条件进行分组进行摆放
-         */
+        //根据制约条件进行分组进行摆放
         Map<Long, List<WorkPriorityOrderRestrictRelation>> relationByGroup = workPriorityOrderRestrictRelations.stream().collect(Collectors.groupingBy(WorkPriorityOrderRestrictRelation::getRestrictCd));
 
+        //根据制约条件进行遍历
         for (Map.Entry<Long, List<WorkPriorityOrderRestrictRelation>> relationEntry : relationByGroup.entrySet()) {
             Long relationCd = relationEntry.getKey();
             //记录商品放到哪里了-同一个制约的商品可能不同的台、段
@@ -1004,6 +1000,7 @@ public class PriorityOrderMstServiceImpl implements PriorityOrderMstService {
                     .stream().filter(data -> relationCd.equals(data.getRestrictCd())).sorted(Comparator.comparingLong(PriorityOrderResultDataDto::getSkuRank)).collect(Collectors.toList());
             List<WorkPriorityOrderRestrictRelation> relationValue = relationEntry.getValue();
 
+            //遍历台、段的制约条件
             for (WorkPriorityOrderRestrictRelation workPriorityOrderRestrictRelation : relationValue) {
                 Integer taiCd = workPriorityOrderRestrictRelation.getTaiCd();
                 Integer tanaCd = workPriorityOrderRestrictRelation.getTanaCd();
@@ -1028,7 +1025,7 @@ public class PriorityOrderMstServiceImpl implements PriorityOrderMstService {
                     width = taiInfo.get().getTaiWidth();
                 }
 
-                //放置商品
+                //遍历商品，开始从上到下，从左到右放置商品
                 for (int i = setResultDataIndex; i < relationSorted.size(); i++) {
                     PriorityOrderResultDataDto priorityOrderResultData = relationSorted.get(i);
 
@@ -1064,6 +1061,7 @@ public class PriorityOrderMstServiceImpl implements PriorityOrderMstService {
             }
         }
 
+        //分组更新，一组1000条
         double batchNum = Math.ceil(finalSetJanResultData.size() / 1000.0);
         for (int i = 0; i < batchNum; i++) {
             //批量更新可以摆放的商品数据到数据库中
