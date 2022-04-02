c0a5c6828e4c47f774c3bcd61ebdddb5591e6bcb
10218504 <10218504chen_ke@cn.tre-inc.com>
2022-04-02 17:02:21 +0800

    【修改】根据authorcd查询是否重名

--- src/main/java/com/trechina/planocycle/service/impl/PriorityOrderMstServiceImpl.java
+++ src/main/java/com/trechina/planocycle/service/impl/PriorityOrderMstServiceImpl.java
@@ -806,7 +806,7 @@ public class PriorityOrderMstServiceImpl implements PriorityOrderMstService {
         String companyCd = priorityOrderMstVO.getCompanyCd();
         String authorCd = session.getAttribute("aud").toString();
 
-        Integer orderNameCount = priorityOrderMstMapper.selectByOrderName(priorityOrderName, companyCd, authorCd);
+        Integer orderNameCount = priorityOrderMstMapper.selectByOrderName(priorityOrderName, authorCd);
         int isEdit = priorityOrderMstMapper.selectByPriorityOrderCd(priorityOrderCd);
 
         if (isEdit > 0) {
