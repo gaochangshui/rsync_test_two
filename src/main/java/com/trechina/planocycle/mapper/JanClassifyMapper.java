b2e4299d7034308cab724feed05621f780510e02
陈克 <10218504chen_ke@cn.tre-inc.com>
2022-05-07 16:52:53 +0800

    【修改】阶层的mapper

---
+++ src/main/java/com/trechina/planocycle/mapper/JanClassifyMapper.java
@@ -0,0 +1,14 @@
+package com.trechina.planocycle.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * prod_0001_jan_kaisou_header_sys
+ */
+@Mapper
+public interface JanClassifyMapper {
+    List<Map<String, String>> selectJanClassify(String tableName);
+}
