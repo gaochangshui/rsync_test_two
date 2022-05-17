f0021ae529e78dcaa019e9411b9d6af4eed4193e
陈克 <10218504chen_ke@cn.tre-inc.com>
2022-05-17 17:30:14 +0800

    【新增】预设定pos项目读取接口

--- src/main/java/com/trechina/planocycle/controller/PresetParamController.java
+++ src/main/java/com/trechina/planocycle/controller/PresetParamController.java
@@ -2,6 +2,7 @@ package com.trechina.planocycle.controller;
 
 import com.trechina.planocycle.service.PresetParamService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,5 +17,10 @@ public class PresetParamController {
     @PostMapping("/setPresetParam")
     public Map<String, Object> setPresetParam(String presetParam){
         return presetParamService.setPresetParam(presetParam);
+    }
+
+    @GetMapping("/getPresetParam")
+    public Map<String, Object> getPresetParam(){
+        return presetParamService.getPresetParam();
     }
 }
