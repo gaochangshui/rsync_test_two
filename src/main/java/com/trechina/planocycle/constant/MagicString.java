de69308cf9390a1834e552aaafd4061114192785
陈克 <10218504chen_ke@cn.tre-inc.com>
2022-05-25 14:00:22 +0800

    【修改】品名1和品名2的编号交换

--- src/main/java/com/trechina/planocycle/constant/MagicString.java
+++ src/main/java/com/trechina/planocycle/constant/MagicString.java
@@ -2,4 +2,12 @@ package com.trechina.planocycle.constant;
 
 public class MagicString {
     public static final String CORE_COMPANY = "core_company";
+    /**
+     * JAN単位-品名2
+     */
+    public static final Integer PRODUCT_TYPE_JAN = 1;
+    /**
+     * SKU単位-品名1
+     */
+    public static final Integer PRODUCT_TYPE_SKU = 2;
 }
