aeb586871a31140c4e6c4aba316eabc0065a1073
陈克 <10218504chen_ke@cn.tre-inc.com>
2022-06-24 10:12:14 +0800

    获取商品展示位置接口

--- src/main/java/com/trechina/planocycle/controller/BasicPatternMstController.java
+++ src/main/java/com/trechina/planocycle/controller/BasicPatternMstController.java
@@ -1,9 +1,7 @@
 package com.trechina.planocycle.controller;
 
 import com.trechina.planocycle.entity.vo.BasicPatternAutoDetectVO;
-import com.trechina.planocycle.enums.ResultEnum;
 import com.trechina.planocycle.service.BasicPatternMstService;
-import com.trechina.planocycle.utils.ResultMaps;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
