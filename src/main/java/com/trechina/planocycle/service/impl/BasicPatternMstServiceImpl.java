5a4ae38b5d23a7be38d2d94859eb882227a736fb
刘鑫宇 <10215814liu_xinyu@cn.tre-inc.com>
2022-06-28 15:57:52 +0800

    修改制约信息

--- src/main/java/com/trechina/planocycle/service/impl/BasicPatternMstServiceImpl.java
+++ src/main/java/com/trechina/planocycle/service/impl/BasicPatternMstServiceImpl.java
@@ -442,17 +442,21 @@ public class BasicPatternMstServiceImpl implements BasicPatternMstService {
         String authorCd = session.getAttribute("aud").toString();
         String companyCd = basicPatternRestrictRelation.getCompanyCd();
         Long priorityOrderCd = basicPatternRestrictRelation.getPriorityOrderCd();
-        restrictRelationMapper.deleteForTanaPosition(basicPatternRestrictRelation);
         if (basicPatternRestrictRelation.getRestrictCd()!= null){
+            restrictRelationMapper.deleteForTanaPosition(basicPatternRestrictRelation);
             restrictRelationMapper.update(basicPatternRestrictRelation,authorCd);
         }else {
             List<BasicPatternRestrictRelation> tanaAttrList = restrictRelationMapper.getTanaAttrList(basicPatternRestrictRelation);
-            if (tanaAttrList.isEmpty()){
-                return ResultMaps.result(ResultEnum.SUCCESS);
-            }
-            int i = 1;
-            for (BasicPatternRestrictRelation patternRestrictRelation : tanaAttrList) {
-                patternRestrictRelation.setTanaPosition(i++);
+            if (tanaAttrList.size()>1){
+                tanaAttrList = tanaAttrList.stream()
+                        .filter(item -> !item.getTanaPosition().equals(basicPatternRestrictRelation.getTanaPosition())).collect(Collectors.toList());
+                int i = 1;
+                for (BasicPatternRestrictRelation patternRestrictRelation : tanaAttrList) {
+                    patternRestrictRelation.setTanaPosition(i++);
+                }
+
+            }else {
+                tanaAttrList.get(0).setRestrictCd(9999L);
             }
             Integer taiCd = Integer.valueOf(basicPatternRestrictRelation.getTaiCd().toString());
             Integer tanaCd = Integer.valueOf(basicPatternRestrictRelation.getTanaCd().toString());
