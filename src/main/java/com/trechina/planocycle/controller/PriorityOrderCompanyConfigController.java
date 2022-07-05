4f729c82467c68da86ac6ac711ceb67e85a1b110
刘鑫宇 <10215814liu_xinyu@cn.tre-inc.com>
2022-07-05 10:09:36 +0800

    保存企业

---
+++ src/main/java/com/trechina/planocycle/controller/PriorityOrderCompanyConfigController.java
@@ -0,0 +1,32 @@
+package com.trechina.planocycle.controller;
+
+import com.trechina.planocycle.entity.po.CompanyConfig;
+import com.trechina.planocycle.service.PriorityOrderCompanyConfigService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+@RestController
+@RequestMapping("/planoCycle/PriorityOrderCompanyConfig")
+public class PriorityOrderCompanyConfigController {
+    @Autowired
+    private PriorityOrderCompanyConfigService priorityOrderCompanyConfigService;
+    /**
+     ** ユーザー選択を保存するエンタープライズ
+     * @return
+     */
+    @PostMapping("saveUserCompany")
+    public Map<String,Object> saveUserCompany(@RequestBody CompanyConfig companyConfig) {
+        return  priorityOrderCompanyConfigService.saveUserCompany(companyConfig);
+    }
+
+
+    /**
+     ** ユーザーが選択したエンタープライズの取得
+     * @return
+     */
+    @GetMapping("getUserCompany")
+    public Map<String,Object> getUserCompany() {
+        return  priorityOrderCompanyConfigService.getUserCompany();
+    }
+}
