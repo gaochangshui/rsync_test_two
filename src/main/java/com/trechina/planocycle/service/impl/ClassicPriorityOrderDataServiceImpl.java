9303f92aa16f4d897c4ea0ac3cd0c48dcf4a9a27
刘鑫宇 <10215814liu_xinyu@cn.tre-inc.com>
2022-08-03 13:23:11 +0800

    修改branch

--- src/main/java/com/trechina/planocycle/service/impl/ClassicPriorityOrderDataServiceImpl.java
+++ src/main/java/com/trechina/planocycle/service/impl/ClassicPriorityOrderDataServiceImpl.java
@@ -265,11 +265,14 @@ public class ClassicPriorityOrderDataServiceImpl implements ClassicPriorityOrder
         List list = new ArrayList<>();
         List<Map<String, Object>> attrName = priorityOrderDataMapper.selectPriorityAttrName(tableNameAttr,isCompanyCd,prodMstClass);
         List<Map<String, Object>> stratumName = priorityOrderDataMapper.selectPriorityStratumName(tableName,isCompanyCd,prodMstClass);
-        attrName = attrName.stream().filter(map->!"6".equals(MapUtils.getString(map,"type"))).collect(Collectors.toList());
-        List<Map<String, Object>> attrName1 = attrName.stream().filter(map->"6".equals(MapUtils.getString(map,"type"))).collect(Collectors.toList());
+        List<Map<String, Object>> attrName1 = attrName.stream().filter(map->"1".equals(MapUtils.getString(map,"type"))
+        || "3".equals(MapUtils.getString(map,"type"))).collect(Collectors.toList());
+        List<Map<String, Object>> attrName2 = attrName.stream().filter(map->!"1".equals(MapUtils.getString(map,"type"))
+                && !"3".equals(MapUtils.getString(map,"type"))).collect(Collectors.toList());
+        attrName2.forEach(map->map.put("attr_type",3));
         list.add(stratumName);
-        list.add(attrName);
         list.add(attrName1);
+        list.add(attrName2);
 
         return ResultMaps.result(ResultEnum.SUCCESS,list);
     }
