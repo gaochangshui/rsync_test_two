f7f89c5a1c239339cd81fc998d6770d7430b22c1
刘鑫宇 <10215814liu_xinyu@cn.tre-inc.com>
2022-08-12 09:04:41 +0800

    修改台棚别属性

--- src/main/java/com/trechina/planocycle/service/impl/PriorityOrderShelfDataServiceImpl.java
+++ src/main/java/com/trechina/planocycle/service/impl/PriorityOrderShelfDataServiceImpl.java
@@ -48,13 +48,9 @@ public class PriorityOrderShelfDataServiceImpl implements PriorityOrderShelfData
     @Autowired
     private BasicPatternRestrictResultMapper restrictResultMapper;
     @Autowired
-    private BasicPatternRestrictRelationMapper restrictRelationMapper;
-    @Autowired
     private WorkPriorityOrderMstMapper workPriorityOrderMstMapper;
     @Autowired
     private ShelfPtsDataMapper shelfPtsDataMapper;
-    @Autowired
-    private PriorityOrderMstAttrSortMapper priorityOrderMstAttrSortMapper;
     @Autowired
     private ZokuseiMstMapper zokuseiMstMapper;
     @Autowired
@@ -144,7 +140,7 @@ public class PriorityOrderShelfDataServiceImpl implements PriorityOrderShelfData
                     .sorted(Comparator.comparing(map -> MapUtils.getInteger(map,"taiCd")))
                     .collect(Collectors.toList());
             Map<String,Object> mapHeader = new HashMap<>();
-            String groupColumns = "taiCd,tanaCd,janCd,janName,plano_depth,plano_height,plano_width,rank,faceNum";
+            String groupColumns = "taiCd,tanaCd,janCd,janName,plano_width,plano_height,plano_depth,rank,faceNum";
             String groupHeader = "台番号,棚段番号,JAN,商品名,幅,高,奥行,RANK,フェース数";
             mapHeader.put("groupColumns",groupColumns);
             mapHeader.put("groupHeader",groupHeader);
