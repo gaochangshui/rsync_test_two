c0d0f3f87d3e2cebe4dee9a0ec37fa306c217c58
刘鑫宇 <10215814liu_xinyu@cn.tre-inc.com>
2022-08-22 15:03:49 +0800

    棚割专用 branch保存和抽出接口

--- src/main/java/com/trechina/planocycle/controller/MstBranchController.java
+++ src/main/java/com/trechina/planocycle/controller/MstBranchController.java
@@ -3,10 +3,7 @@ package com.trechina.planocycle.controller;
 import com.trechina.planocycle.entity.po.BranchList;
 import com.trechina.planocycle.service.MstBranchService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
--- src/main/java/com/trechina/planocycle/service/MstBranchService.java
+++ src/main/java/com/trechina/planocycle/service/MstBranchService.java
@@ -1,14 +1,12 @@
 package com.trechina.planocycle.service;
 
 import com.trechina.planocycle.entity.po.BranchList;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-
 import java.util.Map;
 public interface MstBranchService {
     Map<String,Object> getBranchInfo(BranchList branchList);
-
-    Map<String, Object> setBranchInfo(BranchList branchList);
 
     @Transactional
     Map<String, Object> syncTenData();
