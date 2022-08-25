947cf0a6dbf89ce55621aef395552097e7120f8c
刘鑫宇 <10215814liu_xinyu@cn.tre-inc.com>
2022-08-25 15:56:52 +0800

    棚割专用 branch保存和抽出接口

--- src/main/java/com/trechina/planocycle/service/impl/TableTransferServiceImpl.java
+++ src/main/java/com/trechina/planocycle/service/impl/TableTransferServiceImpl.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.annotation.PostConstruct;
 import java.util.List;
 
 @Service
@@ -88,7 +87,6 @@ public class TableTransferServiceImpl implements TableTransferService {
 
     @Override
     @Transactional(rollbackFor = Exception.class)
-    @PostConstruct
     public void syncZokuseiMst() {
         String syncCompanyList = sysConfigMapper.selectSycConfig("sync_company_list");
         String[] companyList = syncCompanyList.split(",");
