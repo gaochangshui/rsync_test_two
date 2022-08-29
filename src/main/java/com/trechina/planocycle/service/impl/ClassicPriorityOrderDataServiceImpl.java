38b10518aa29b185c3d64b57124462d7d15c891c
10218504 <10218504chen_ke@cn.tre-inc.com>
2022-08-29 13:00:06 +0800

    【修改】再利用扩缩报错的问题

--- src/main/java/com/trechina/planocycle/service/impl/ClassicPriorityOrderDataServiceImpl.java
+++ src/main/java/com/trechina/planocycle/service/impl/ClassicPriorityOrderDataServiceImpl.java
@@ -468,7 +468,7 @@ public class ClassicPriorityOrderDataServiceImpl implements ClassicPriorityOrder
             priorityOrderCatepakMapper.setWorkForFinal(workCatepakId, companyCd, priorityOrderCd,newPriorityOrderCd);
 
             priorityOrderCatepakAttributeMapper.deleteByPrimaryKey(companyCd, newPriorityOrderCd);
-            priorityOrderCatepakAttributeMapper.setWorkForFinal(companyCd, priorityOrderCd,newPriorityOrderCd);
+            priorityOrderCatepakAttributeMapper.setWorkForFinal(companyCd, priorityOrderCd,newPriorityOrderCd, workCatepakId);
 
             priorityOrderMapper.delete(newPriorityOrderCd);
             priorityOrderMapper.insertWork(companyCd, priorityOrderCd,newPriorityOrderCd);
