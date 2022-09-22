3acd79237064e7cb9abdf666a8f092337bb6e5bb
10218504 <10218504chen_ke@cn.tre-inc.com>
2022-09-22 09:26:23 +0800

    【update】修改店铺名重复少一个pts出力的问题

--- src/main/java/com/trechina/planocycle/mapper/PriorityOrderPtsPatternNameMapper.java
+++ src/main/java/com/trechina/planocycle/mapper/PriorityOrderPtsPatternNameMapper.java
@@ -7,11 +7,12 @@ import java.util.Map;
 
 @Mapper
 public interface PriorityOrderPtsPatternNameMapper {
-    int insertPtsPatternName(Integer priorityOrderCd, Integer shelfNameCd, List<Map<String, Object>> newJanList);
+    int insertPtsPatternName(Integer priorityOrderCd, Integer shelfNameCd, List<Map<String, Object>> newJanList,
+                             Integer patternCd, String branchList);
 
     int deletePtsPatternName();
 
-    List<String> selectExistPatternJan(Integer priorityOrderCd, Integer shelfNameCd, List<String> newPtsJanCdList);
+    List<String> selectExistPatternJan(Integer priorityOrderCd, Integer shelfNameCd, List<String> newPtsJanCdList, String branchList);
 
     void deletePtsPatternNameByCd(Integer priorityOrderCd);
 }
