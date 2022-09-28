b7cbc01cfcaa08d938c0e9d8296d977aca4fc56c
10218504 <10218504chen_ke@cn.tre-inc.com>
2022-09-28 13:22:56 +0800

    【update】修改同期val为null失败的情况

--- src/main/java/com/trechina/planocycle/service/ZokuseiMstDataService.java
+++ src/main/java/com/trechina/planocycle/service/ZokuseiMstDataService.java
@@ -1,5 +1,10 @@
 package com.trechina.planocycle.service;
 
+import java.util.List;
+import java.util.Map;
+
 public interface ZokuseiMstDataService {
     void syncZokuseiMstData(String companyCd, String classCd);
+
+    void setZokuseiData(String company, String classCd, Integer zokuseiId, Integer col, List<Map<String, Object>> headerMap);
 }
