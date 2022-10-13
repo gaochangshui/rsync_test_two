5ecf19e2f28e600dcc7b6f4510f53581af0e3201
10218504 <10218504chen_ke@cn.tre-inc.com>
2022-10-13 14:13:22 +0800

    【update】新增发邮件提醒工具类

--- src/main/java/com/trechina/planocycle/aspect/GlobalExceptionHandler.java
+++ src/main/java/com/trechina/planocycle/aspect/GlobalExceptionHandler.java
@@ -1,10 +1,15 @@
 package com.trechina.planocycle.aspect;
 
+import cn.hutool.extra.mail.MailAccount;
 import com.alibaba.fastjson.JSON;
+import com.trechina.planocycle.config.MailConfig;
 import com.trechina.planocycle.enums.ResultEnum;
+import com.trechina.planocycle.utils.MailUtils;
 import com.trechina.planocycle.utils.ResultMaps;
+import org.apache.commons.collections4.MapUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,11 +20,16 @@ import java.util.Map;
 @Component
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    @Value("projectIds")
+    private String projectIds;
     private Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
     @ExceptionHandler(Exception.class)
     @ResponseBody
     public Map<String,Object> handlerException(Exception e){
         logger.error("",e);
+
+        MailAccount account = MailConfig.getMailAccount(!projectIds.equals("nothing"));
+        MailUtils.sendEmail(account, "10218504chen_ke@cn.tre-inc.com", "test","testtest");
         return ResultMaps.error(ResultEnum.FAILURE, JSON.toJSONString(e));
     }
 }
