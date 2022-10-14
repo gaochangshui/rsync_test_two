bd8e71f4b8182ac6e7babf75c6848a5d3a4d4da2
刘鑫宇 <10215814liu_xinyu@cn.tre-inc.com>
2022-10-14 09:07:36 +0800

    添加錯誤信息發送郵件

--- src/test/java/com/trechina/planocycle/PlanoCycleApiApplicationTests.java
+++ src/test/java/com/trechina/planocycle/PlanoCycleApiApplicationTests.java
@@ -168,41 +168,6 @@ class PlanoCycleApiApplicationTests {
 
     @Test
     public  void test8()  {
-        //String emailHost = "";  //发件人主机
-        //String transportType = ""; //发送协议
-        //String fromUser = ""; //发件人名称
-        //String fromEmail = ""; //发件人邮箱
-        //String authCode = ""; //发件人邮箱授权码
-        //try {
-        //    HtmlEmail email = new HtmlEmail();
-        //    List<String> list = new ArrayList<String>();
-        //    list.add("address");
-        //    String[] tos = list.toArray(new String[list.size()]);
-        //
-        //    // 这里是SMTP发送服务器的名字：163的如下："smtp.163.com"
-        //    email.setHostName("smtp.exmail.qq.com");
-        //    if (isSSL) {
-        //        email.setSSLOnConnect(true);
-        //        email.setSmtpPort(465);
-        //    }
-        //    // 字符编码集的设置
-        //    email.setCharset("UTF-8");
-        //    // 收件人的邮箱
-        //    email.addTo(tos);
-        //    // 发送人的邮箱以及发件人名称
-        //    email.setFrom("XXX@163.com", "zhanghan");
-        //    // 如果需要认证信息的话，设置认证：用户名-密码。分别为发件人在邮件服务器上的注册名称和密码
-        //    email.setAuthentication("XXX@163.com", "yyyy");
-        //    // 要发送的邮件主题
-        //    email.setSubject("subject");
-        //    // 要发送的信息，由于使用了HtmlEmail，可以在邮件内容中使用HTML标签
-        //    email.setHtmlMsg("htmlMsg");
-        //
-        //    String result1 = email.send();
-        //
-        //} catch (Exception e) {
-        //    e.printStackTrace();
-        //}
 
     }
 }
