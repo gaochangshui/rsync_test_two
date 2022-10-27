27d2c81f239719f42a105188fd27913494e42cf9
10218504 <10218504chen_ke@cn.tre-inc.com>
2022-10-27 15:37:09 +0800

    【update】修改企业共通配置接口参数类型

--- src/main/java/com/trechina/planocycle/service/impl/ParamConfigServiceImpl.java
+++ src/main/java/com/trechina/planocycle/service/impl/ParamConfigServiceImpl.java
@@ -299,6 +299,8 @@ public class ParamConfigServiceImpl implements ParamConfigService {
         List<Map<String, Object>> janUnitList = allParamConfigVO.getJanUnit();
         sysConfigMapper.updateVal(janUnitList);
 
+        sysConfigMapper.updateValByName(MagicString.LEVEL, allParamConfigVO.getLevel());
+
         return ResultMaps.result(ResultEnum.SUCCESS);
     }
 }
