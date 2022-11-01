4c0af85b168d4365482d053891be68549ca7220b
刘鑫宇 <10215814liu_xinyu@cn.tre-inc.com>
2022-11-01 11:12:15 +0800

    添加返回企業class接口

--- src/main/java/com/trechina/planocycle/controller/SysConfigController.java
+++ src/main/java/com/trechina/planocycle/controller/SysConfigController.java
@@ -3,10 +3,7 @@ package com.trechina.planocycle.controller;
 import com.trechina.planocycle.entity.dto.EnterpriseAxisDto;
 import com.trechina.planocycle.service.SysConfigService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -22,7 +19,7 @@ public class SysConfigController {
     }
 
 
-    @PostMapping("/getCompanySettings")
+    @GetMapping("/getCompanySettings")
     public Map<String, Object> getCompanySettings(String companyCd){
         return sysConfigService.getCompanySettings(companyCd);
     }
